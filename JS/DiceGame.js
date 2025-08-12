450e77ee036dc609c7056f51b75fe7e0f6fee802
Võ Thành Lên <75373744+LenVoIT@users.noreply.github.com>
2025-08-12 23:37:01 +0700

    Learning JQuery and push SimonGame.html + css

--- JS/DiceGame.js
+++ JS/DiceGame.js
@@ -1,24 +1,41 @@
 // const randomNumber1 = Math.floor(Math.random() * 6) + 1;
-const rollDice = document
-  .getElementById("RollDice")
-  .addEventListener("click", () => {
-    const rollDice1 = Math.floor(Math.random() * 6) + 1;
-    const rollDice2 = Math.floor(Math.random() * 6) + 1;
+// Xử lý sự kiện khi nhấn nút RollDice
+document.getElementById("RollDice").addEventListener("click", () => {
+  // Sinh số ngẫu nhiên cho mỗi người chơi
+  const rollDice1 = Math.floor(Math.random() * 6) + 1;
+  const rollDice2 = Math.floor(Math.random() * 6) + 1;
 
-    //update img as the number of dice
-    document.getElementById("dice1").src = `/DiceImages/dice${rollDice1}.png`;
-    document.getElementById("dice2").src = `/DiceImages/dice${rollDice2}.png`;
+  // Cập nhật hình ảnh xúc xắc
+  document.getElementById("dice1").src = `/DiceImages/dice${rollDice1}.png`;
+  document.getElementById("dice2").src = `/DiceImages/dice${rollDice2}.png`;
 
-    console.log(`Player 1 rolled: ${rollDice1}`);
-    console.log(`Player 2 rolled: ${rollDice2}`);
+  // Hiển thị kết quả ra console
+  console.log(`Player 1 rolled: ${rollDice1}`);
+  console.log(`Player 2 rolled: ${rollDice2}`);
 
-    const title = document.getElementById("result");
-    console.log(title);
-    if (rollDice1 > rollDice2) {
-      title.textContent = "Player 1 win";
-    } else if (rollDice1 == rollDice2) {
-      title.textContent = "Draw";
-    } else {
-      title.textContent = "Player 2 win";
-    }
-  });
+  // Hiển thị kết quả lên giao diện
+  const title = document.getElementById("result");
+  if (rollDice1 > rollDice2) {
+    title.textContent = "Player 1 win";
+  } else if (rollDice1 === rollDice2) {
+    title.textContent = "Draw";
+  } else {
+    title.textContent = "Player 2 win";
+  }
+});
+// $("#RollDice").css("cursor","none");
+$('#JQueryBtn').click(function() {
+  $('#message').text('Bạn vừa click nút!');
+});
+$('#JQueryBtn').hover(
+  function() {
+    $('#message').text('Bạn đang hover nút!');
+  },
+  function() {
+    $('#message').text('Bạn vừa rời khỏi nút!');
+  }
+);
+
+$(document).keypress(function(event){
+  $(`h1`).text(event.key);
+})
